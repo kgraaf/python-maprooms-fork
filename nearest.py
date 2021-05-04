059c8945213fc22b94c97b436209677558810106
Igor Khomyakov <ikh@ikhsoftware.com>
2021-05-04 16:20:06 -0400

    Stand alone implementation and test

---
+++ nearest.py
@@ -0,0 +1,31 @@
+from typing import Tuple, List
+import numpy as np
+
+
+def nearest(
+    input_grids: List[Tuple[float, float]],  # [(x0, dx), (y0, dy), ...]
+    input_data: np.ndarray,
+    output_grids: List[np.ndarray],  # [x, y, ...]
+) -> np.ndarray:
+    padded_data = np.pad(input_data, pad_width=1, mode="constant", constant_values=np.nan)
+    index = tuple(
+        np.minimum(np.maximum(((x - (x0 - 1.5 * dx)) / dx).astype(int), 0), n - 1)
+        for (x0, dx), x, n in zip(input_grids, output_grids, padded_data.shape)
+    )
+    return padded_data[tuple(reversed(np.meshgrid(*reversed(index))))]
+    # return padded_data[tuple(np.meshgrid(*index))].T
+
+
+input_grids = [(1, 0.25), (2, 0.5)]
+input_data = np.array([[10.0, 20.0, 30], [40.0, 50.0, 60.0]])
+output_grids = [np.asarray([1.20, 1.25, 1.38]), np.asarray([1.95, 2.05, 2.1, 3.1])]
+
+# input_grids = [(1, 0.25), (2, 0.5)]
+# input_data = np.array([[10.0, 20.0, 30], [40.0, 50.0, 60.0]])
+# output_grids = [np.asarray([1, 1.25]), np.asarray([2, 2.5, 3])]
+
+print("*** input_grids:", input_grids)
+print("*** output_grids:", output_grids)
+print("*** input_data:", input_data, input_data.shape)
+z = nearest(input_grids, input_data, output_grids)
+print("*** z:", z)
