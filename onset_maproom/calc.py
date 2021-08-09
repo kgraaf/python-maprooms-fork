80cca13d1f4f6f8d2a4645e81a34d840acfb4ac1
remic <remic@iri.columbia.edu>
2021-08-09 10:15:10 -0400

    onset_date now expects a time grid named T zarr file was regenerated with ingrid conventions X, Y, T

--- onset_maproom/calc.py
+++ onset_maproom/calc.py
@@ -11,9 +11,9 @@ def read_zarr_data(zarr_path):
 def onset_date(dailyRain, early_start, search_days, rainy_day, running_days, running_total, min_rainy_days, dry_days, dry_spell):
   onset_date = xr.Dataset()
   onset_date["onset_date"] = dailyRain.precip[
-    (dailyRain['time'].dt.day==int(early_start.partition(" ")[0]))
+    (dailyRain['T'].dt.day==int(early_start.partition(" ")[0]))
     &
-    (dailyRain['time'].dt.strftime("%b")==early_start.partition(" ")[2])
+    (dailyRain['T'].dt.strftime("%b")==early_start.partition(" ")[2])
   ]
   onset_date["onset_date"] = xr.DataArray(
     data=np.random.randint(0, high=search_days, size=onset_date["onset_date"].shape).astype('timedelta64[D]'),
@@ -23,5 +23,5 @@ def onset_date(dailyRain, early_start, search_days, rainy_day, running_days, run
         description="Onset Date",
     ),
   )
-  onset_date["onset_date"] = onset_date["time"] + onset_date["onset_date"]  
+  onset_date["onset_date"] = onset_date["T"] + onset_date["onset_date"]  
   return onset_date
