d2aad9c13ee2207a2822e23f1d198ed9796af3e5
Drew Resnick <drewr@iri.columbia.edu>
2021-09-30 15:46:21 -0400

    add tolerance to nearest neighbor looup (data res = 0.0375)

--- onset_maproom/maproom.py
+++ onset_maproom/maproom.py
@@ -107,7 +107,7 @@ def map_click(click_lat_lng):
 def onset_plots(click_lat_lng, search_start_day, search_start_month, searchDays, wetThreshold,
                 runningDays, runningTotal, minRainyDays, dryDays,drySpell):
     lat, lng = get_coords(click_lat_lng)
-    precip = rr_mrg.precip.sel(X=lng, Y=lat, method="nearest")
+    precip = rr_mrg.precip.sel(X=lng, Y=lat, method="nearest", tolerance=0.05)
     precip.load()
     onset_delta = calc.seasonal_onset_date(precip, int(search_start_day),
         calc.strftimeb2int(search_start_month), int(searchDays),
