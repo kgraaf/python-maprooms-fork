2eeebfe70fce4b1a8d77e324cc4157f436875d8f
remic <remic@iri.columbia.edu>
2021-10-14 07:52:13 -0400

    dry spell past the dry spell search window don't invalidate 6

--- onset_maproom/tests/test_calc.py
+++ onset_maproom/tests/test_calc.py
@@ -2008,6 +2008,11 @@ def test_onset():
         0,
         precip,
     )
+    preciplateDS = xr.where(
+        (precip["T"] > (pd.to_datetime("2000-05-09") + pd.Timedelta(days=20))),
+        0,
+        precip,
+    )
 
     onsets = calc.onset_date(
         daily_rain=precip,
@@ -2036,8 +2041,18 @@ def test_onset():
         dry_spell_length=7,
         dry_spell_search=21,
     )
+    onsetslateDS = calc.onset_date(
+        daily_rain=preciplateDS,
+        wet_thresh=1,
+        wet_spell_length=3,
+        wet_spell_thresh=20,
+        min_wet_days=1,
+        dry_spell_length=7,
+        dry_spell_search=21,
+    )
     assert pd.Timedelta(onsets.values) == pd.Timedelta(days=6)
     assert pd.Timedelta(onsetsDS.values) != pd.Timedelta(days=6)
+    assert pd.Timedelta(onsetslateDS.values) == pd.Timedelta(days=6)
     # Converting to pd.Timedelta doesn't change the meaning of the
     # assertion, but gives a more helpful error message when the test
     # fails: Timedelta('6 days 00:00:00')
