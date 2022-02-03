977c3dd6f3c501d195115d79b3e1cf807bff7131
Kevin Graaf <82181542+kgraaf@users.noreply.github.com>
2022-02-03 15:20:20 -0500

    added error message when no data available

--- fbfmaproom/fbfmaproom.py
+++ fbfmaproom/fbfmaproom.py
@@ -739,6 +739,7 @@ def display_prob_thresh(val):
     Output("table", "columns"),
     Output("summary", "columns"),
     Output("prob_thresh", "value"),
+    Output("command_msg", "children"),
     Input("issue_month", "value"),
     Input("freq", "value"),
     Input("mode", "value"),
@@ -764,9 +765,9 @@ def _(issue_month_idx, freq, mode, geom_key, pathname, severity, obs_dataset_key
             geom_key,
             severity,
         )
-        return dft.to_dict("records"), dfs.to_dict("records"), tcs, tcs, prob_thresh
+        return dft.to_dict("records"), dfs.to_dict("records"), tcs, tcs, prob_thresh, ""
     except Exception:
-        return None, None, None, None, None
+        return None, None, None, None, None, "no data available for the selected location"
 
 
 @APP.callback(
