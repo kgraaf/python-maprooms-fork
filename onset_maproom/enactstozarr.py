50c67cffee6cdaa38b4bc283363746b8061c746e
Aaron Kaplan <aaron@iri.columbia.edu>
2022-03-31 12:08:02 -0400

    Comment about different interpolation methods

--- onset_maproom/enactstozarr.py
+++ onset_maproom/enactstozarr.py
@@ -34,6 +34,15 @@ rr_mrg = xr.open_mfdataset(
     preprocess = set_up_dims,
     parallel=False
 ).precip.interp(
+    # TODO this method of regridding is inaccurate because it pretends
+    # that (X, Y) define a Euclidian space. In reality, grid cells
+    # farther from the equator cover less area and thus should be
+    # weighted less heavily. Also, consider using conservative
+    # interpolation instead of bilinear, since when going to a much
+    # coarser resoution, bilinear discards a lot of information. See [1],
+    # and look into xESMF.
+    #
+    # [1] https://climatedataguide.ucar.edu/climate-data-tools-and-analysis/regridding-overview
     X=np.arange(33, 48 + RESOLUTION, RESOLUTION),
     Y=np.arange(3, 15 + RESOLUTION, RESOLUTION),
 ).chunk(chunks={'T': 600, 'Y': 24, 'X': 30})
