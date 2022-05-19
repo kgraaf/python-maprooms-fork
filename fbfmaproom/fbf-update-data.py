470ef5e87e48c92f3ae3a2f83f44de22bb5f8ffb
Aaron Kaplan <aaron@iri.columbia.edu>
2022-05-19 09:20:37 -0400

    Get Niger ENACTS from /home/.aaron, updated with 2020-21

--- fbfmaproom/fbf-update-data.py
+++ fbfmaproom/fbf-update-data.py
@@ -63,7 +63,7 @@ datasets = [
     ),
     (
         'niger/enacts-precip-jas',
-        'http://iridl.ldeo.columbia.edu/home/.audreyv/.Niger/.ENACTS/.monthly/.rainfall/.CHIRPS/.rfe_merged/T/(1991)/last/RANGE/T/(Jul-Sep)/seasonalAverage/3/mul///name//obs/def/'
+        'http://iridl.ldeo.columbia.edu/home/.aaron/.Niger/.ENACTS/.monthly/.rainfall/.CHIRPS/.rfe_merged/T/(1991)/last/RANGE/T/(Jul-Sep)/seasonalAverage/3/mul///name//obs/def/'
     ),
     (
         'niger/chirps-precip-jun',
