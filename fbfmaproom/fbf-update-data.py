335c2f59f216297e875ea309bca5474f540d4a99
Aaron Kaplan <aaron@iri.columbia.edu>
2022-06-22 15:51:15 -0400

    New Ethiopia-OND forecast

--- fbfmaproom/fbf-update-data.py
+++ fbfmaproom/fbf-update-data.py
@@ -124,8 +124,8 @@ url_datasets = [
         "http://iridl.ldeo.columbia.edu/SOURCES/.UCSB/.CHIRPS/.v2p0/.daily-improved/.global/.0p25/.prcp/30/mul/X/39.875/47.875/RANGE/Y/3.625/10.875/RANGE/T/(Oct-Dec)/seasonalAverage//units/(mm/month)/def/",
     ),
     (
-        "pnep-ethiopia-ond",
-        "http://iridl.ldeo.columbia.edu/home/.aaron/.Ethiopia/.CPT/.NextGen/.OND_PRCP/.Somali/.NextGen/.FbF/.pne/P//P//percentile/0/5/5/95/NewEvenGRID/replaceGRID/",
+        "ethiopia/pnep-ond",
+        "http://iridl.ldeo.columbia.edu/home/.remic/.NMA/.NextGen/.OND_PRCP/.Ethiopia/.NextGen/.FbF/.pne/P//P//percentile/0/5/5/95/NewEvenGRID/replaceGRID/"
     ),
     (
         'niger/enacts-precip-jas',
