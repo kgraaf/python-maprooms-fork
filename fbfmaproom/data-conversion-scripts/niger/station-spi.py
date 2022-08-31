10afb3bb7655ed0c5303d3fb64ad2c1a5096a11f
Aaron Kaplan <aaron@iri.columbia.edu>
2022-08-31 19:48:37 -0400

    Relative path

--- fbfmaproom/data-conversion-scripts/niger/station-spi.py
+++ fbfmaproom/data-conversion-scripts/niger/station-spi.py
@@ -11,4 +11,4 @@ data_fixed = pd.read_excel(fname, sheet_name="array and analysis", usecols="AS",
 data['Average'][data.index[-1]] = data_fixed['Unnamed: 44'][data_fixed.index[-1]]
 t = pd.read_excel(fname, sheet_name="array and analysis", usecols="AR", skiprows=42, nrows=32)
 T = t.apply(lambda y: cftime.datetime(y, 8, 16, calendar='360_day'), axis=1)
-xr.Dataset(data.rename(T)).rename_dims({'dim_0': 'T'}).rename_vars({'dim_0': 'T'}).to_zarr("/data/<<DATADIR>>/niger/station-spi-jj.zarr")
+xr.Dataset(data.rename(T)).rename_dims({'dim_0': 'T'}).rename_vars({'dim_0': 'T'}).to_zarr("niger-station-spi-jj.zarr")
