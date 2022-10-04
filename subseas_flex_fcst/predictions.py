59a49e4ce0a15b49af7567a88dbd6c52527f93ea
drewmresnick <drewr@iri.columbia>
2022-10-04 12:42:21 -0400

    add some documentation to funcs

--- subseas_flex_fcst/predictions.py
+++ subseas_flex_fcst/predictions.py
@@ -9,6 +9,27 @@ import pandas as pd
 
 #select file for specific lead time and start date
 def sel_cpt_file(data_path, file_pattern, leadTime, startDate):
+    """ Select a single cpt file for a given start and lead.
+
+    Parameters
+    ----------
+    data_path : str
+        String of the path pointing to cpt datasets.
+    file_pattern : str
+        String of the file pattern name for a given variable's data file.
+    leadTime : str
+         String of the lead time value to be selected for as is represented in the file name.
+    startDate : str
+        String of the start date to be selected for as is represented in the file name.
+    Returns
+    -------
+    fileSelected : xarray Dataset
+        Single CPT data file as multidimensional xarray dataset.
+    Notes
+    -----
+    `file_pattern` should be most common denominator for any group of datasets,
+    so that a sinlge file can be selected using only `leadTime` and `startDate`.
+    """
     pattern = f"{startDate}_{leadTime}"
     fullPath = f"{data_path}/{file_pattern}"
     fileName = fullPath.replace("*",pattern)
@@ -19,8 +40,27 @@ def sel_cpt_file(data_path, file_pattern, leadTime, startDate):
 
 
 def target_range_format(leads_value,leads_key,start_date,period_length):
-    ''' Formatting target range using leads and starts, and target range period length.
-    '''
+    """ Formatting target range using leads and starts, and target range period length.
+
+    Parameters
+    ----------
+    leads_value : int
+        Application's integer representation of lead time.
+    leads_key : str
+        Provider's representation of lead time in the data.
+    start_date : Timestamp
+        Start date for the forecast.
+    period_length : int
+       Length of forecast target range period.
+    Returns
+    -------
+    date_range : str
+        String of target date range.
+    Notes
+    -----
+    If the providers representation of lead time is an integer value, convert to str for input.
+    The function will output the most concise version of the date range depending on if years and months are equal.
+    """
     target_start = start_date + timedelta(days=leads_value)
     target_end = target_start + timedelta(days= period_length - 1)
     if (target_start).strftime("%Y") == (target_end).strftime("%Y"):
@@ -36,6 +76,32 @@ def target_range_format(leads_value,leads_key,start_date,period_length):
 
 
 def cpt_starts_list(data_path,file_pattern,regex_search_pattern,strftime_format="%b-%-d-%Y"):
+    """ Get list of all start dates from CPT files.
+
+    Parameters
+    ----------
+    data_path : str
+        String of the path pointing to cpt datasets.
+    file_pattern : str
+        String of the file pattern name for a given variable's data file.
+    regex_search_pattern : str
+        String representing regular expression search pattern to find dates in file names.
+    strftime_format : str
+        String representing output dates format.
+    Returns
+    -------
+    start_dates : list
+        List of strings representing all start dates for the data within `data_path`.
+    Notes
+    -----
+    For more information on regex visit: https://docs.python.org/3/library/re.html
+    Test your regex code here: https://regexr.com/
+    Examples
+    --------
+    Regex expression "\w{3}-\w{1,2}-\w{4}" matches expressions that are:
+    '{word of 3 chars}-{word between 1,2 chars}-{word of 4 chars}'
+    will match dates of format 'Apr-4-2022', 'dec-14-2022', etc.
+    """
     files_name_list = glob.glob(f'{data_path}/{file_pattern}')
     start_dates = []
     for file in files_name_list:
