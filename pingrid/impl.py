19d50d44080fdd1e034fba188f205e7ae257a9ee
remic <remic@iri.columbia.edu>
2022-10-13 15:54:37 -0400

    use min/max on first and last only

--- pingrid/impl.py
+++ pingrid/impl.py
@@ -64,10 +64,10 @@ def sel_snap(spatial_array, lat, lng, dim_y="Y", dim_x="X"):
     the_method = None
     half_res_y = np.abs(spatial_array[dim_y][1] - spatial_array[dim_y][0]) / 2
     half_res_x = np.abs(spatial_array[dim_x][1] - spatial_array[dim_x][0]) / 2
-    min_y = spatial_array[dim_y].min() - half_res_y
-    max_y = spatial_array[dim_y].max() + half_res_y
-    min_x = spatial_array[dim_x].min() - half_res_x
-    max_x = spatial_array[dim_x].max() + half_res_x
+    min_y = spatial_array[dim_y][[0, -1]].min()
+    max_y = spatial_array[dim_y][[0, -1]].max()
+    min_x = spatial_array[dim_x][[0, -1]].min()
+    max_x = spatial_array[dim_x][[0, -1]].max()
     if lat >= min_y and lat <= max_y and lng >= min_x and lng <= max_x:
         the_method = "nearest"
     return spatial_array.sel(method=the_method, **{dim_x:lng, dim_y:lat})
