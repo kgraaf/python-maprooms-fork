4aeb4b94f32a2163bd3bcb86986a33256c8e4066
Rémi Cousin <42619415+remicousin@users.noreply.github.com>
2022-10-17 15:38:04 -0400

    passing the bucket into a loop (#203)
    * passing the bucket into a loop
    
    * dim is now time_dim, might be ambigous when we introduce year. we'll see
    
    * collapsing expand_dims and assign_coords
    
    * _bucket is now called _step

--- onset_maproom/agronomy.py
+++ onset_maproom/agronomy.py
@@ -1,10 +1,12 @@
 import xarray as xr
+import numpy as np
 
-def soil_plant_water_bucket(
+
+def soil_plant_water_step(
     sm_yesterday,
     peffective,
     et,
-    taw
+    taw,
 ):
     """Compute soil-plant-water balance from yesterday to today.
     The balance is thought as a bucket with water coming in and out:
@@ -29,7 +31,7 @@ def soil_plant_water_bucket(
     peffective : DataArray
         effective precipitation today.
     et : DataArray
-        evapotransipiration of the plant today.
+        evapotranspiration of the plant today.
     taw : DataArray
         total available water that represents the maximum water capacity of the soil.
         
@@ -37,11 +39,72 @@ def soil_plant_water_bucket(
     -------
     sm, drainage : Tuple of DataArray
         today soil moisture and drainage
+        
+    See Also
+    --------
+    soil_plant_water_balance
     
     """
     
     # Water Balance
-    wb = sm_yesterday + peffective - et
+    wb = (sm_yesterday + peffective - et).clip(min=0)
     drainage = (wb - taw).clip(min=0)
     sm = wb - drainage
+    return sm, drainage
+
+
+def soil_plant_water_balance(
+    peffective,
+    et,
+    taw,
+    sminit,
+    time_dim="T",
+):
+    """Compute soil-plant-water balance day after day over a growing season.
+    See `soil_plant_water_step` for the step by step algorithm definition.
+    
+    Parameters
+    ----------
+    peffective : DataArray
+        daily effective precipitation.
+    et : DataArray
+        daily evapotranspiration of the plant.
+    taw : DataArray
+        total available water that represents the maximum water capacity of the soil.
+    sminit : DataArray
+        timeless soil moisture to initialize the loop with.
+    time_dim : str, optional
+        daily time dimension to run the balance against (default `time_dim` ="T").
+        
+    Returns
+    -------
+    sm, drainage : Tuple of DataArray
+        daily soil moisture and drainage over the growing season.
+        
+    See Also
+    --------
+    soil_plant_water_step
+    
+    """
+    
+    # First Step
+    if np.size(et) == 1:
+        et = xr.DataArray(et)
+    sm0, drainage0 = soil_plant_water_step(
+        sminit,
+        peffective.isel({time_dim: 0}, drop=True),
+        et.isel({time_dim: 0}, missing_dims='ignore', drop=True),
+        taw,
+    )
+    # Give time dimension to sm and drainage    
+    sm = sm0.expand_dims({time_dim: peffective[time_dim]}).copy()
+    drainage = drainage0.expand_dims({time_dim: peffective[time_dim]}).copy()
+    # Filling/emptying bucket day after day
+    for doy in range(1, peffective[time_dim].size):
+        sm[{time_dim: doy}], drainage[{time_dim: doy}] = soil_plant_water_step(
+            sm.isel({time_dim: doy - 1}),
+            peffective.isel({time_dim: doy}),
+            et.isel({time_dim: doy}, missing_dims='ignore'),
+            taw,
+        )
     return sm, drainage
--- onset_maproom/tests/test_agronomy.py
+++ onset_maproom/tests/test_agronomy.py
@@ -1,14 +1,16 @@
 import xarray as xr
 import agronomy
+import pandas as pd
+import numpy as np
 
 
-def test_spwb_basic():
+def test_spwbu_basic():
 
     sm_previous_day = xr.DataArray(30)
     peffective = xr.DataArray(10)
     et = xr.DataArray(5)
     taw = xr.DataArray(60)
-    sm, drainage = agronomy.soil_plant_water_bucket(
+    sm, drainage = agronomy.soil_plant_water_step(
         sm_previous_day,
         peffective,
         et,
@@ -25,7 +27,7 @@ def test_spwb_with_dims_and_drainage():
     peffective = xr.DataArray([10, 10])
     et = xr.DataArray([5, 5])
     taw = xr.DataArray([60, 60])
-    sm, drainage = agronomy.soil_plant_water_bucket(
+    sm, drainage = agronomy.soil_plant_water_step(
         sm_previous_day,
         peffective,
         et,
@@ -36,3 +38,50 @@ def test_spwb_with_dims_and_drainage():
     assert (sm == [35, 60]).all()
 
 
+def test_spwba_basic():
+    
+    sm, drainage = agronomy.soil_plant_water_balance(
+        precip_sample(),
+        5,
+        60,
+        10,
+    )
+    expected = [
+        5.054383,  0.054383,  0.      ,  0.      ,  0.      ,  0.      ,
+        2.763758,  1.043278,  9.419212,  8.691078, 15.856108, 20.562167,
+        22.610772, 17.610772, 12.610772,  7.610772,  3.483541,  1.649589,
+        0.      ,  0.      ,  0.      ,  0.      ,  1.474878,  0.      ,
+        0.      ,  0.      ,  4.029134,  0.      ,  0.      ,  0.      ,
+        0.      ,  0.      ,  0.      ,  0.      ,  0.      ,  0.      ,
+        0.      ,  0.      ,  0.      ,  0.      ,  0.      ,  0.      ,
+        0.      ,  0.      ,  0.      ,  0.      ,  0.239006,  0.      ,
+        0.      ,  0.      ,  0.      ,  0.      ,  0.      ,  0.      ,
+        5.737132,  1.335959,  0.      ,  0.      , 13.794922, 12.622312,
+        10.350632
+    ]
+    
+    assert np.allclose(sm, expected)
+    assert np.allclose(drainage, 0)
+        
+
+def precip_sample():
+
+    t = pd.date_range(start="2000-05-01", end="2000-06-30", freq="1D")
+    # this is rr_mrg.isel(X=0, Y=124, drop=True).sel(T=slice("2000-05-01", "2000-06-30"))
+    # fmt: off
+    values = [
+        0.054383,  0.      ,  0.      ,  0.027983,  0.      ,  0.      ,
+        7.763758,  3.27952 , 13.375934,  4.271866, 12.16503 ,  9.706059,
+        7.048605,  0.      ,  0.      ,  0.      ,  0.872769,  3.166048,
+        0.117103,  0.      ,  4.584551,  0.787962,  6.474878,  0.      ,
+        0.      ,  2.834413,  9.029134,  0.      ,  0.269645,  0.793965,
+        0.      ,  0.      ,  0.      ,  0.191243,  0.      ,  0.      ,
+        4.617332,  1.748801,  2.079067,  2.046696,  0.415886,  0.264236,
+        2.72206 ,  1.153666,  0.204292,  0.      ,  5.239006,  0.      ,
+        0.      ,  0.      ,  0.      ,  0.679325,  2.525344,  2.432472,
+        10.737132,  0.598827,  0.87709 ,  0.162611, 18.794922,  3.82739 ,
+        2.72832
+    ]
+    # fmt: on
+    precip = xr.DataArray(values, dims=["T"], coords={"T": t})
+    return precip
